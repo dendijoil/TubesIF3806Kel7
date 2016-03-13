5c66bacce6545bd8b1a18278b23a692e0799235c
dendijoil <dendijoil@gmail.com>
2016-03-13 22:56:50 +0800

    Update Barang.java

--- ApplikasiBarang/src/applikasibarang/Barang.java
+++ ApplikasiBarang/src/applikasibarang/Barang.java
@@ -1,1 +1,26 @@
 
+public class Barang{
+	private long id;
+	private String nama;
+
+	public Barang(long id,String nama){
+		this.id=id;
+		this.nama=nama;
+	}
+	
+	public void setId(long id){
+		this.id = id;
+	}
+
+	public void setNama(String nama){
+		this.nama = nama;
+	}
+
+	public long getId (){
+		return id;
+	}
+
+	public String getNama(){
+		return nama;
+	}
+}
