d19e30bebba2497c33a5531eef97a58867b51514
dendijoil <dendijoil@gmail.com>
2016-03-13 22:45:31 +0800

    Update Mobil.java

--- ApplikasiBarang/src/applikasibarang/Mobil.java
+++ ApplikasiBarang/src/applikasibarang/Mobil.java
@@ -1,1 +1,27 @@
 
+public class Mobil extends Barang{
+	private int cc;
+	private String merk;
+
+	public Mobil(long id,String nama,int cc,String merk){
+		super(id,nama);
+		this.cc=cc;
+		this.merk=merk;
+	}
+	
+	public void setCc(int cc){
+		this.cc = cc;
+	}
+
+	public void setMerk(String merk){
+		this.merk = merk;
+	}
+
+	public int getCc (){
+		return cc;
+	}
+
+	public String getMerk(){
+		return merk;
+	}
+}
