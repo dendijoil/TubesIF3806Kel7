ac23dc21605d0f0e2d405c6b479c457ede710e5c
shasaorryza <shasaorryza13@gmail.com>
2016-03-14 10:09:44 +0700

    Update Petugas.java
    Nama : Orryza Dwi Juni Ayuningtyas
    NIM : 1301144416
    Kelas : IF 38-06

--- ApplikasiBarang/src/applikasibarang/Petugas.java
+++ ApplikasiBarang/src/applikasibarang/Petugas.java
@@ -1,1 +1,29 @@
-
+public class Petugas extends Orang{
+	private long id;
+	private String nama;
+	
+	public Petugas(String nama, long id){
+		super(nama);
+		this.id=id;
+	}
+	
+	public void setNama(String nama){
+		this.nama=nama;
+	}
+	
+	public String getNama(){
+		return nama;
+	}
+	
+	public void setId(long id){
+		this.id=id;
+	}
+	
+	public long getId(){
+		return id;
+	}
+	
+	public String jenisOtoritas(){
+		return "Petugas";
+	}
+}
