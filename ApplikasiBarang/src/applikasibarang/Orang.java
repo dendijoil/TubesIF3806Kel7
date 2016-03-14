f79511f285c3925ec8313f8999d5cf7006dd7316
shasaorryza <shasaorryza13@gmail.com>
2016-03-14 10:07:42 +0700

    Update Orang.java
    Nama : Orryza Dwi Juni Ayuningtyas
    NIM : 1301144416
    Kelas : IF 38-06

--- ApplikasiBarang/src/applikasibarang/Orang.java
+++ ApplikasiBarang/src/applikasibarang/Orang.java
@@ -1,1 +1,17 @@
-
+public abstract class Orang{
+	private String nama;
+	
+	public Orang(String nama){
+		this.nama=nama;
+	}
+	
+	public void setNama(String nama){
+		this.nama=nama;
+	}
+	
+	public String getNama(){
+		return nama;
+	}
+	
+	public abstract String jenisOtoritas();
+}
