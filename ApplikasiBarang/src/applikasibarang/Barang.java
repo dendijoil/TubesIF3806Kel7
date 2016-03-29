e47abb7dcfa5939cc7c0564d2a9ce97f74c19f49
dendijoil <dendijoil@gmail.com>
2016-03-29 14:33:25 +0800

    Update Barang.java

--- ApplikasiBarang/src/applikasibarang/Barang.java
+++ ApplikasiBarang/src/applikasibarang/Barang.java
@@ -1,11 +1,15 @@
 
+package applikasibarang;
+
 public class Barang{
 	private long id;
 	private String nama;
+        private int jenis;
 
-	public Barang(long id,String nama){
+	public Barang(long id,String nama,int jenis){
 		this.id=id;
 		this.nama=nama;
+                this.jenis=jenis;
 	}
 	
 	public void setId(long id){
@@ -22,5 +26,18 @@ public class Barang{
 
 	public String getNama(){
 		return nama;
+	}
+        
+        public void setjenis(int jenis){
+		this.jenis = jenis;
+	}
+
+	public String getjenis (){
+		if(jenis==1){
+                    return "Makanan";
+                }
+                else{
+                    return "Bukan Makanan";
+                }
 	}
 }
